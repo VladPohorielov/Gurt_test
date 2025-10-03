2ea9a6c83e887a9c812b25b4c31ba569706653f4
Vlad <vladpog2015@gmail.com>
2025-10-03 11:04:19 +0300

    Move GitHub Pages files to root: index.html, assets, images for direct hosting

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 // Deploy note: when publishing to GitHub Pages under the repository
 // https://github.com/VladPohorielov/Gurt_test set base to '/Gurt_test/'
 export default defineConfig({
-	base: '/Gurt_test/',
-	plugins: [react()],
-	build: {
-		outDir: 'docs'
-	}
+  base: '/Gurt_test/',
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  }
 })
