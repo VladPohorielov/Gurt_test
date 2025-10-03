176663906564107860e0a02dc0f9ef0d55d337eb
Vlad <vladpog2015@gmail.com>
2025-10-03 10:51:33 +0300

    Fix: add GitHub Pages SPA routing support with 404.html and basename

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ const root = createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <CartProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/Gurt_test">
         <App />
       </BrowserRouter>
     </CartProvider>
